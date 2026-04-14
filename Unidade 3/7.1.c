fb6ad8f9e721d8cdd29c89419e39f8845a7201d6
mandypoli <amandiholiv@gmail.com>
2026-04-14 15:38:07 -0300

    chore: sincroniza estrutura local e remove diretorios fantasmas

--- Unidade 3/7.1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-//Desenvolva uma função que receba um número e retorne verdadeiro se for par e falso
-//caso contrário.
-
-bool ehPar(int numero) {
-    return (numero % 2 == 0);
-}
--- Unidade 3/8.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-//Crie um algoritmo que utilize uma função para calcular o quadrado de um número e exiba
-//o resultado.
-
-
